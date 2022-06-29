21ae1506723bde7f67afe84e6a86ba19114535f6
adan villalpando <adan_1990.icloud.com>
2022-06-29 14:15:08 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.get('/css',(req,res) => {
+  res.sendFile(path.join(__dirname, '../styles.css'))
+})
+
+app.get('/css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../styles.css'))
+})
+
+const port = process.env.PORT || 4005
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
